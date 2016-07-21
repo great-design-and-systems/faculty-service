2d157f57d30f8fcd0eb73831cae52db63a662263
rickzx98 <rickzx98@gmail.com>
2016-07-22 04:29:00 +0800

    updates

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var Server = require('./src/config/server');
 var Resource = require('./src/boundary/faculty-resource');
 var express = require('express');
 var app = express();
+var LoggerServer = require('./src/config/logger-server');
 
 (function() {
     new Database();
     new Server(app);
     new Resource(app);
+    new LoggerServer(app);
 })();
 
 module.exports = app;
